07289fb91f4931dc0ec05ba68392b8def14fdfc8
markcyr <markcyr@msn.com>
2017-05-04 14:20:51 -0400

    bb 1

--- election.js
+++ election.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
   // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json',
+  }).done(function(Data){
+      for (var i = 0; i < Data.candidates.length; i++) {
+          candidate = Data.candidates[i];
 
+          var candidateInformation =  (' has recieved ' + candidate.votes + 'votes.');
+          var strongTag = $('<strong>').html(candidate.name);
+          var liTag = $('<li>').append(strongTag).append(candidateInformation);
+          $('#bob').append(liTag);
+        }
+  });
 });
